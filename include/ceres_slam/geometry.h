413fa55119d331db746bb7ae41d6f49533fab7d2
Lee Clement <lee.clement@robotics.utias.utoronto.ca>
2015-12-03 11:46:03 -0500

    Fixed a couple errors and made templated typenames more meaninful

--- include/ceres_slam/geometry.h
+++ include/ceres_slam/geometry.h
@@ -6,12 +6,12 @@
 #include <sstream>
 
 #include <Eigen/Core>
-#include <Eigen/Eigenvalues>
+#include <Eigen/Eigenvalues> // For SO(3) log
 
 namespace ceres_slam {
 
 //! Base class for homogeneous points/vectors
-template <typename T>
+template <typename Scalar>
 class Homogeneous3D {
 public:
     //! Pointer type
@@ -21,32 +21,32 @@ public:
     //! Dimension of the space
     static const int dim = 3;
     //! Euclidean type
-    typedef Eigen::Matrix<T, dim, 1> CartesianType;
+    typedef Eigen::Matrix<Scalar, dim, 1> CartesianScalar;
     //! Homogeneous type
-    typedef Eigen::Matrix<T, dim+1, 1> HomogeneousType;
+    typedef Eigen::Matrix<Scalar, dim+1, 1> HomogeneousScalar;
 
     //! Default constructor
-    Homogeneous3D() : Homogeneous3D(HomogeneousType::Zero()) { }
+    Homogeneous3D() : Homogeneous3D(HomogeneousScalar::Zero()) { }
     //! Copy constructor
     Homogeneous3D( const Homogeneous3D& other ) :
         Homogeneous3D(other.homogeneous) { }
     //! Construct from a 4-vector
-    Homogeneous3D( const HomogeneousType& vector ) :
+    Homogeneous3D( const HomogeneousScalar& vector ) :
         Homogeneous3D(vector.head(3), vector(3)) { }
     //! Construct from a 3-vector and a scalar
-    Homogeneous3D( const CartesianType& epsilon, const T eta ) :
+    Homogeneous3D( const CartesianScalar& epsilon, const Scalar eta ) :
         _epsilon(epsilon), _eta(eta) { }
     //! Construct from 4 scalars
-    Homogeneous3D( T x, T y, T z, T w ) :
-        _epsilon(CartesianType(x, y, z)), _eta(w) { }
+    Homogeneous3D( Scalar x, Scalar y, Scalar z, Scalar w ) :
+        _epsilon(CartesianScalar(x, y, z)), _eta(w) { }
 
     //! Return the cartesian form of the point/vector
-    const CartesianType cartesian() const { return _epsilon; }
+    const CartesianScalar cartesian() const { return _epsilon; }
     //! Return the homogeneous scale part of the point/vector
-    const T scale() const { return _eta; }
+    const Scalar scale() const { return _eta; }
     //! Return the homogeneous form of the point
-    const HomogeneousType homogeneous() const {
-        HomogeneousType h;
+    const HomogeneousScalar homogeneous() const {
+        HomogeneousScalar h;
         h.head(3) = cartesian();
         h(3) = scale();
         return h;
@@ -54,52 +54,54 @@ public:
 
     //! Ostream operator for homogeneous quantities
     friend std::ostream& operator<<(
-        std::ostream& os, const Homogeneous3D<T>& h ) {
+        std::ostream& os, const Homogeneous3D<Scalar>& h ) {
         os << "Homogeneous quantity" << std::endl
            << h.homogeneous() << std::endl;
         return os;
     }
 protected:
     //! Cartesian part
-    CartesianType _epsilon;
+    CartesianScalar _epsilon;
     //! Homogeneous scale part
-    T _eta;
+    Scalar _eta;
 };
 
 //! Vector in 3D space
-template <typename T>
-class Vector3D : public Homogeneous3D<T> {
+template <typename Scalar>
+class Vector3D : public Homogeneous3D<Scalar> {
 public:
     //! Pointer type
     typedef std::shared_ptr<Vector3D> Ptr;
     //! Const pointer type
     typedef const std::shared_ptr<Vector3D> ConstPtr;
     //! Cartesian type
-    typedef typename Homogeneous3D<T>::CartesianType CartesianType;
+    typedef typename Homogeneous3D<Scalar>::CartesianScalar CartesianScalar;
     //! Homogeneous type
-    typedef typename Homogeneous3D<T>::HomogeneousType HomogeneousType;
+    typedef typename Homogeneous3D<Scalar>::HomogeneousScalar HomogeneousScalar;
 
     //! Default constructor
-    Vector3D() : Vector3D(CartesianType::Zero()) { }
+    Vector3D() : Vector3D(CartesianScalar::Zero()) { }
     //! Copy constructor
     Vector3D(const Vector3D& other) : Vector3D(other.cartesian()) { }
     //! Construct from a 3-vector
-    Vector3D(const CartesianType& epsilon) :
-        Homogeneous3D<T>(epsilon, 0.) { }
+    Vector3D(const CartesianScalar& epsilon) :
+        Homogeneous3D<Scalar>(epsilon, 0.) { }
     //! Construct from 3 scalars
-    Vector3D( T i, T j, T k ) : Homogeneous3D<T>(i, j, k, 0.) { }
+    Vector3D( Scalar i, Scalar j, Scalar k ) :
+        Homogeneous3D<Scalar>(i, j, k, 0.) { }
 
     //! Addition operator for two vectors
-    const Vector3D operator+( const Vector3D<T>& other ) const {
+    const Vector3D operator+( const Vector3D<Scalar>& other ) const {
         return Vector3D( this->cartesian() + other.cartesian() );
     }
     //! Subtraction operator for two vectors
-    const Vector3D operator-( const Vector3D<T>& other ) const {
+    const Vector3D operator-( const Vector3D<Scalar>& other ) const {
         return Vector3D( this->cartesian() - other.cartesian() );
     }
 
     //! Ostream operator for homogeneous quantities
-    friend std::ostream& operator<<( std::ostream& os, const Vector3D<T>& v ) {
+    friend std::ostream& operator<<( std::ostream& os,
+        const Vector3D<Scalar>& v ) {
         os << "Homogeneous vector" << std::endl
            << v.homogeneous() << std::endl;
         return os;
@@ -107,43 +109,45 @@ public:
 };
 
 //! Point in 3D space
-template <typename T>
-class Point3D : public Homogeneous3D<T> {
+template <typename Scalar>
+class Point3D : public Homogeneous3D<Scalar> {
 public:
     //! Pointer type
     typedef std::shared_ptr<Point3D> Ptr;
     //! Const pointer type
     typedef const std::shared_ptr<Point3D> ConstPtr;
     //! Cartesian type
-    typedef typename Homogeneous3D<T>::CartesianType CartesianType;
+    typedef typename Homogeneous3D<Scalar>::CartesianScalar CartesianScalar;
     //! Homogeneous type
-    typedef typename Homogeneous3D<T>::HomogeneousType HomogeneousType;
+    typedef typename Homogeneous3D<Scalar>::HomogeneousScalar HomogeneousScalar;
 
     //! Default constructor
-    Point3D() : Point3D(CartesianType::Zero(), 1.) { }
+    Point3D() : Point3D(CartesianScalar::Zero()) { }
     //! Copy constructor
     Point3D(const Point3D& other) : Point3D(other.cartesian()) { }
     //! Construct from a 3-vector
-    Point3D(const CartesianType& epsilon) :
-        Homogeneous3D<T>(epsilon, 1.) { }
+    Point3D(const CartesianScalar& epsilon) :
+        Homogeneous3D<Scalar>(epsilon, 1.) { }
     //! Construct from 3 scalars
-    Point3D( T x, T y, T z ) : Homogeneous3D<T>(x, y, z, 1.) { }
+    Point3D( Scalar x, Scalar y, Scalar z ) :
+        Homogeneous3D<Scalar>(x, y, z, 1.) { }
 
     //! Addition operator for point and vector
-    const Point3D operator+( const Vector3D<T>& other ) const {
+    const Point3D operator+( const Vector3D<Scalar>& other ) const {
         return Point3D( this->cartesian() + other.cartesian() );
     }
     //! Subtraction operator for point and vector
-    const Point3D operator-( const Vector3D<T>& other ) const {
+    const Point3D operator-( const Vector3D<Scalar>& other ) const {
         return Point3D( this->cartesian() - other.cartesian() );
     }
     //! Subtraction operator for point and point
-    const Vector3D operator-( const Point3D<T>& other ) const {
-        return Vector3D( this->cartesian() - other.cartesian() );
+    const Vector3D<Scalar> operator-( const Point3D& other ) const {
+        return Vector3D<Scalar>( this->cartesian() - other.cartesian() );
     }
 
     //! Ostream operator for homogeneous quantities
-    friend std::ostream& operator<<( std::ostream& os, const Point3D<T>& p ) {
+    friend std::ostream& operator<<( std::ostream& os,
+        const Point3D<Scalar>& p ) {
         os << "Homogeneous point" << std::endl
            << p.homogeneous() << std::endl;
         return os;
@@ -151,7 +155,7 @@ public:
 };
 
 //! SO3 Lie group (3D rotations)
-template <typename T>
+template <typename Scalar>
 class SO3 {
 public:
     //! Pointer type
@@ -163,17 +167,17 @@ public:
     //! Dimension of transformation matrix
     static const int dim = 3;
     //! Point type
-    typedef Point3D<T> Point;
+    typedef Point3D<Scalar> Point;
     //! Vector type
-    typedef Vector3D<T> Vector;
+    typedef Vector3D<Scalar> Vector;
     //! Group transformation type
-    typedef Eigen::Matrix<T, dim, dim> TransformationMatrix;
+    typedef Eigen::Matrix<Scalar, dim, dim> TransformationMatrix;
     //! Tangent vector type
-    typedef Eigen::Matrix<T, dof, 1> TangentVector;
+    typedef Eigen::Matrix<Scalar, dof, 1> TangentVector;
     //! Adjoint transformation type
-    typedef Eigen::Matrix<T, dof, dof> AdjointMatrix;
+    typedef Eigen::Matrix<Scalar, dof, dof> AdjointMatrix;
     //! Transformed point Jacobian matrix
-    typedef Eigen::Matrix<T, dim, dof> TransformedPointJacobian;
+    typedef Eigen::Matrix<Scalar, dim, dof> TransformedPointJacobian;
 
     //! Default constructor (identity)
     SO3() : SO3(TransformationMatrix::Identity()) { }
@@ -184,9 +188,9 @@ public:
     //! Matrix exponential: rotation matrix from axis-angle tangent vector
     inline
     static const SO3 exp( const TangentVector& a ) {
-        T phi = a.norm();
-        T cp = cos(phi);
-        T sp = sin(phi);
+        Scalar phi = a.norm();
+        Scalar cp = cos(phi);
+        Scalar sp = sin(phi);
 
         TangentVector a_unit = a / phi;
 
@@ -261,13 +265,14 @@ public:
     //! SO(3) transformed point Jacobian
     inline
     static const TransformedPointJacobian transformed_point_jacobian(
-        const Homogeneous3D<T>& h) {
+        const Homogeneous3D<Scalar>& h) {
         TransformedPointJacobian result = -wedge(h.cartesian());
         return result;
     }
 
     //! Ostream operator for SO(3)
-    friend std::ostream& operator<<( std::ostream& os, const SO3<T>& so3 ) {
+    friend std::ostream& operator<<( std::ostream& os,
+        const SO3<Scalar>& so3 ) {
         os << "SO(3) Rotation Matrix" << std::endl << so3.matrix() << std::endl;
         return os;
     }
@@ -278,7 +283,7 @@ private:
 };
 
 //! SE3 Lie group (3D rigid body transformations)
-template <typename T>
+template <typename Scalar>
 class SE3 {
 public:
     //! Pointer type
@@ -290,19 +295,19 @@ public:
     //! Dimension of transformation matrix
     static const int dim = 4;
     //! Point type
-    typedef Point3D<T> Point;
+    typedef Point3D<Scalar> Point;
     //! Vector type
-    typedef Vector3D<T> Vector;
+    typedef Vector3D<Scalar> Vector;
     //! Rotation type
-    typedef SO3<T> Rotation;
+    typedef SO3<Scalar> Rotation;
     //! Group transformation type
-    typedef Eigen::Matrix<T, dim, dim> TransformationMatrix;
+    typedef Eigen::Matrix<Scalar, dim, dim> TransformationMatrix;
     //! Tangent vector type
-    typedef Eigen::Matrix<T, dof, 1> TangentVector;
+    typedef Eigen::Matrix<Scalar, dof, 1> TangentVector;
     //! Adjoint transformation type
-    typedef Eigen::Matrix<T, dof, dof> AdjointMatrix;
+    typedef Eigen::Matrix<Scalar, dof, dof> AdjointMatrix;
     //! Transformed (Cartesian) point Jacobian matrix
-    typedef Eigen::Matrix<T, dim, dof> TransformedPointJacobian;
+    typedef Eigen::Matrix<Scalar, dim, dof> TransformedPointJacobian;
 
     //! Default constructor (identity)
     SE3() : SE3(TransformationMatrix::Identity()) { }
@@ -379,7 +384,7 @@ public:
     inline
     static const TransformationMatrix wedge( const TangentVector& xi ) {
         TransformationMatrix Xi;
-        Xi.block(0,0,3,3) = SO3<T>::wedge(xi.tail(3));
+        Xi.block(0,0,3,3) = SO3<Scalar>::wedge(xi.tail(3));
         Xi.block(0,3,3,1) = xi.head(3);
         Xi.bottomRows(1) << 0., 0., 0., 0.;
         return Xi;
@@ -398,7 +403,7 @@ public:
     //! (for a homogeneous point/vector p = [epsilon^T eta]^T)
     inline
     static const TransformedPointJacobian transformed_point_jacobian(
-        const Homogeneous3D<T>& h ) {
+        const Homogeneous3D<Scalar>& h ) {
         TransformedPointJacobian result;
         result.block(0,0,3,3) =
             h.scale() * Rotation::TransformationMatrix::Identity();
@@ -409,7 +414,7 @@ public:
     }
 
     //! Ostream operator for SE(3)
-    friend std::ostream& operator<<(std::ostream& os, const SE3<T>& se3) {
+    friend std::ostream& operator<<(std::ostream& os, const SE3<Scalar>& se3) {
         os << "SE(3) Transformation Matrix" << std::endl << se3.matrix() << std::endl;
         return os;
     }
