0c2c4dfd2a0a48da2ed79b4d07fd84664e0caff9
Lee Clement <lee.clement@robotics.utias.utoronto.ca>
2016-01-25 12:55:25 -0500

    Added SO3 perturbation for ceres

--- include/ceres_slam/perturbations.h
+++ include/ceres_slam/perturbations.h
@@ -7,6 +7,42 @@
 
 namespace ceres_slam {
 
+//! SO(3) group perturbation via so(3) algebra
+class SO3Perturbation {
+public:
+    template <typename T>
+    bool operator()(const T* C_op_ceres,
+                    const T* psi_ceres,
+                    T* C_op_new_ceres) const {
+        // Local typedefs for convenience
+        typedef SO3Group<T> SO3T;
+        typedef typename SO3T::TangentVector TangentVectorT;
+
+        // Initial unperturbed transformation
+        Eigen::Map<const SO3T> C_op(C_op_ceres);
+        // The perturbation in the transformation's tangent space
+        Eigen::Map<const TangentVectorT> psi(psi_ceres);
+        // Final perturbed transformation
+        Eigen::Map<SO3T> C_op_new(C_op_new_ceres);
+
+        // Perturb the transformation
+        C_op_new = SO3T::exp(psi) * C_op;
+
+        return true;
+    }
+
+    //! Factory to hide the construction of the LocalParameterization object
+    //! from the client code.
+    static ceres::LocalParameterization* Create() {
+        return(new ceres::AutoDiffLocalParameterization
+                <SO3Perturbation,
+                    9, // SO(3) matrix elements
+                    3> // so(3) tangent vector
+              );
+    }
+}; // class SE3Perturbation
+
+//! SE(3) group perturbation via se(3) algebra
 class SE3Perturbation {
 public:
     template <typename T>
@@ -30,7 +66,7 @@ public:
         return true;
     }
 
-    //! Factort to hide the construction of the LocalParameterization object
+    //! Factory to hide the construction of the LocalParameterization object
     //! from the client code.
     static ceres::LocalParameterization* Create() {
         return(new ceres::AutoDiffLocalParameterization
@@ -41,6 +77,6 @@ public:
     }
 }; // class SE3Perturbation
 
-}
+} // namespace ceres_slam
 
 #endif // CERES_SLAM_PERTURBATIONS_H_
